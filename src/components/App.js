0e88050cadf34085df152027a368edcbf782cb95
Sandra Fernandz <sandraenmunich@gmail.com>
2021-10-02 08:24:59 +0200

    adding filter

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,8 @@ import '../styles/App.scss';
 
 function App() {
   const [data, setData] = useState([]);
+  const [searchName, setSearchName] = useState('');
+
   useEffect(() => {
     getCharactersFromApi().then((initialData) => {
       console.log(initialData);
@@ -12,16 +14,37 @@ function App() {
     });
   }, []);
 
+  {
+    /*función manejadora del input*/
+  }
+
+  const handleChangeSearchName = (ev) => {
+    setSearchName(ev.currentTarget.value);
+  };
+
+  {
+    /*función para filtrar por nombre y pasamos datos filtrados a map en CharactersList para que los pinte*/
+  }
+
+  const filteredData = data.filter((character) =>
+    character.name.toLocaleLowerCase().includes(searchName.toLocaleLowerCase())
+  );
   return (
     <div>
       <h1>Rick and Morty</h1>
       <section>
         <form action="">
-          <input type="text" />
+          <label htmlFor="name">Encuentra un personaje</label>
+          <input
+            type="text"
+            id="name"
+            value={searchName}
+            onChange={handleChangeSearchName}
+          />
         </form>
       </section>
       <section>
-        <CharactersList data={data} />
+        <CharactersList data={filteredData} />
       </section>
     </div>
   );
