0057da51d92306cf7425e263d7363b1f71255545
Sandra Fernandz <sandraenmunich@gmail.com>
2021-10-04 14:39:10 +0200

    reversing FilterByName component creation

---
+++ src/components/FilterByName.js
@@ -0,0 +1,16 @@
+function FilterByName(props) {
+  return (
+    <>
+      <label htmlFor="name">Encuentra un personaje</label>
+      <input
+        className="form_input"
+        type="text"
+        id="name"
+        value={props.searchName}
+        onChange={props.handleChangeSearchName}
+      />
+    </>
+  );
+}
+
+export default FilterByName;
