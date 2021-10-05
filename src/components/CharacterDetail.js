19cda7c70ec39be773b90858a82c863c63c40c38
Sandra Fernandz <sandraenmunich@gmail.com>
2021-10-05 09:03:45 +0200

    adding back to home link to character detail

--- src/components/CharacterDetail.js
+++ src/components/CharacterDetail.js
@@ -1,15 +1,37 @@
 import '../styles/characterDetail.scss';
+import { Link } from 'react-router-dom';
+import arrow from '../images/arrow.png';
 
 function CharacterDetail(props) {
   if (props.character === undefined) {
     return (
       <section className="notFound">
         <p>Ese personaje no existe</p>
+        <p className="notFound_container">
+          <Link to="/" className="notFound_link">
+            Volver
+            <img
+              src={arrow}
+              className="notFound_arrow"
+              alt="Flecha para regresar a página principal"
+            />
+          </Link>
+        </p>
       </section>
     );
   } else {
     return (
       <section className="detail">
+        <p className="detail_container">
+          <img
+            src={arrow}
+            className="detail_arrow"
+            alt="Flecha para regresar a página principal"
+          />
+          <Link className="detail_container_link" to="/">
+            Volver
+          </Link>
+        </p>
         <img
           src={props.character.image}
           alt={`Esta es la foto de ${props.character.name}`}
